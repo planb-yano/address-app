08e93131d060cf0eb94f3ae2dc51a685b940ac2f
planb-yano <takahiro.yano@plan-b.co.jp>
2023-06-18 23:30:41 +0900

    all done

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0418MxDbOfHXNihWSd0bvWA-a_bBD3iM",
+  authDomain: "address-app-180b7.firebaseapp.com",
+  projectId: "address-app-180b7",
+  storageBucket: "address-app-180b7.appspot.com",
+  messagingSenderId: "933988680713",
+  appId: "1:933988680713:web:31c18995d41c8eebf864d8",
+  measurementId: "G-JR8G6LGVK6",
+};
+
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,109 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import firebase from "firebase";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    drawer: false,
+    addresses: [],
+    login_user: null,
   },
   getters: {
+    displayName: (state) =>
+      state.login_user ? state.login_user.displayName : "",
+    photoURL: (state) => (state.login_user ? state.login_user.photoURL : ""),
+    uid: (state) => (state.login_user ? state.login_user.uid : ""),
+    getAddressById: (state) => (id) =>
+      state.addresses.find((address) => address.id === id),
   },
   mutations: {
+    setLoginUser(state, user) {
+      state.login_user = user;
+    },
+    deleteLoginUser(state) {
+      state.login_user = null;
+    },
+    toggleSideMenu(state) {
+      state.drawer = !state.drawer;
+    },
+    addAddress(state, { id, address }) {
+      address.id = id;
+      state.addresses.push(address);
+    },
+    updateAddress(state, { id, address }) {
+      const index = state.addresses.findIndex((address) => address.id === id);
+      state.addresses[index] = address;
+    },
+    deleteAddress(state, { id }) {
+      const index = state.addresses.findIndex((address) => address.id === id);
+      state.addresses.splice([index], 1);
+    },
   },
   actions: {
+    setLoginUser({ commit }, user) {
+      commit("setLoginUser", user);
+    },
+    deleteLoginUser({ commit }) {
+      commit("deleteLoginUser");
+    },
+    logout() {
+      firebase.auth().signOut();
+    },
+    login() {
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(google_auth_provider);
+    },
+    fetchAddresses({ getters, commit }) {
+      firebase
+        .firestore()
+        .collection(`users/${getters.uid}/addresses`)
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) =>
+            commit("addAddress", { id: doc.id, address: doc.data() })
+          );
+        });
+    },
+    toggleSideMenu({ commit }) {
+      commit("toggleSideMenu");
+    },
+    addAddress({ getters, commit }, address) {
+      if (getters.uid) {
+        firebase
+          .firestore()
+          .collection(`users/${getters.uid}/addresses`)
+          .add(address)
+          .then((doc) => {
+            commit("addAddress", { id: doc.id, address });
+          });
+      }
+    },
+    updateAddress({ getters, commit }, { id, address }) {
+      if (getters.uid) {
+        firebase
+          .firestore()
+          .collection(`users/${getters.uid}/addresses`)
+          .doc(id)
+          .update(address)
+          .then(() => {
+            commit("updateAddress", { id, address });
+          });
+      }
+    },
+    deleteAddress({ getters, commit }, { id }) {
+      if (getters.uid) {
+        firebase
+          .firestore()
+          .collection(`users/${getters.uid}/addresses`)
+          .doc(id)
+          .delete()
+          .then(() => {
+            commit("deleteAddress", { id });
+          });
+      }
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
